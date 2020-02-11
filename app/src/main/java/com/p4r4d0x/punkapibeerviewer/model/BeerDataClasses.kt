1caef80cf0ae996d4fe950a6f96ef623fde030f1
Albrath <aloarter@gmail.com>
2020-02-11 23:30:01 +0100

    Created the model data classes to hold the json data.
     * ADD: Data classes in BeerDataClasses to hold the json values
     * ADD: Test for the parse from/to json/class over the BeerDTO

---
+++ app/src/main/java/com/p4r4d0x/punkapibeerviewer/model/BeerDataClasses.kt
@@ -0,0 +1,143 @@
+package com.p4r4d0x.punkapibeerviewer.model
+
+
+import com.google.gson.annotations.SerializedName
+
+
+data class Temp(
+    @SerializedName("unit")
+    val unit: String = "",
+    @SerializedName("value")
+    val value: Int = 0
+)
+
+
+data class HopsItem(
+    @SerializedName("add")
+    val add: String = "",
+    @SerializedName("amount")
+    val amount: Amount,
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("attribute")
+    val attribute: String = ""
+)
+
+
+data class Volume(
+    @SerializedName("unit")
+    val unit: String = "",
+    @SerializedName("value")
+    val value: Int = 0
+)
+
+
+data class MashTempItem(
+    @SerializedName("duration")
+    val duration: Int = 0,
+    @SerializedName("temp")
+    val temp: Temp
+)
+
+
+data class Amount(
+    @SerializedName("unit")
+    val unit: String = "",
+    @SerializedName("value")
+    val value: Double = 0.0
+)
+
+
+data class Ingredients(
+    @SerializedName("hops")
+    val hops: List<HopsItem>?,
+    @SerializedName("yeast")
+    val yeast: String = "",
+    @SerializedName("malt")
+    val malt: List<MaltItem>?
+)
+
+data class BeerDtoWrapper(
+    val listBeer: List<BeerDTO>
+)
+
+data class BeerDTO(
+    @SerializedName("first_brewed")
+    val firstBrewed: String = "",
+    @SerializedName("attenuation_level")
+    val attenuationLevel: Int = 0,
+    @SerializedName("method")
+    val method: Method,
+    @SerializedName("target_og")
+    val targetOg: Int = 0,
+    @SerializedName("image_url")
+    val imageUrl: String = "",
+    @SerializedName("boil_volume")
+    val boilVolume: BoilVolume,
+    @SerializedName("ebc")
+    val ebc: Double = 0.0,
+    @SerializedName("description")
+    val description: String = "",
+    @SerializedName("target_fg")
+    val targetFg: Int = 0,
+    @SerializedName("srm")
+    val srm: Int = 0,
+    @SerializedName("volume")
+    val volume: Volume,
+    @SerializedName("contributed_by")
+    val contributedBy: String = "",
+    @SerializedName("abv")
+    val abv: Double = 0.0,
+    @SerializedName("food_pairing")
+    val foodPairing: List<String>?,
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("ph")
+    val ph: Double = 0.0,
+    @SerializedName("tagline")
+    val tagline: String = "",
+    @SerializedName("ingredients")
+    val ingredients: Ingredients,
+    @SerializedName("id")
+    val id: Int = 0,
+    @SerializedName("ibu")
+    val ibu: Int = 0,
+    @SerializedName("brewers_tips")
+    val brewersTips: String = ""
+)
+
+
+data class MaltItem(
+    @SerializedName("amount")
+    val amount: Amount,
+    @SerializedName("name")
+    val name: String = ""
+)
+
+
+data class Method(
+    @SerializedName("mash_temp")
+    val mashTemp: List<MashTempItem>?,
+    @SerializedName("fermentation")
+    val fermentation: Fermentation,
+    @SerializedName("twist")
+    val twist: String
+)
+
+
+data class Fermentation(
+    @SerializedName("temp")
+    val temp: Temp
+)
+
+
+data class BoilVolume(
+    @SerializedName("unit")
+    val unit: String = "",
+    @SerializedName("value")
+    val value: Int = 0
+)
+
+
+
+
---
+++ app/src/test/java/com/p4r4d0x/punkapibeerviewer/DTOBeerUnitTest.kt
@@ -0,0 +1,45 @@
+package com.p4r4d0x.punkapibeerviewer
+
+import com.google.gson.Gson
+import com.p4r4d0x.punkapibeerviewer.TestingConstants.Companion.TEST_DTO_BEER_JSON_VALUE_EMPTY
+import com.p4r4d0x.punkapibeerviewer.TestingConstants.Companion.TEST_DTO_BEER_JSON_VALUE_FEW_ELEMENTS
+import com.p4r4d0x.punkapibeerviewer.model.BeerDTO
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+
+
+class DTOBeerUnitTest {
+
+
+    lateinit var gson: Gson
+    @Before
+    fun setup() {
+        gson = Gson()
+    }
+
+
+    @Test
+    fun jsonParse_fewElements() {
+        //Parse from json into a class
+        val dtoBeerRead: List<BeerDTO> =
+            gson.fromJson(TEST_DTO_BEER_JSON_VALUE_FEW_ELEMENTS, Array<BeerDTO>::class.java)
+                .toList()
+        assertNotNull(dtoBeerRead)
+        //Parse back from the class to the json value
+        val jsonDtoBeerRead = gson.toJson(dtoBeerRead)
+        assertEquals(TEST_DTO_BEER_JSON_VALUE_FEW_ELEMENTS, jsonDtoBeerRead)
+    }
+
+    @Test
+    fun jsonParse_empty() {
+        //Parse from json into a class
+        val dtoBeerRead: List<BeerDTO> =
+            gson.fromJson(TEST_DTO_BEER_JSON_VALUE_EMPTY, Array<BeerDTO>::class.java).toList()
+        assertNotNull(dtoBeerRead)
+        //Parse back from the class to the json value
+        val jsonDtoBeerRead = gson.toJson(dtoBeerRead)
+        assertEquals(TEST_DTO_BEER_JSON_VALUE_EMPTY, jsonDtoBeerRead)
+    }
+}
